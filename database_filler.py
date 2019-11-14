f5ddb0e1cbff2ed78c9aa402b87c64e6b94f1901
cizmarf <cizmarfilip@gmail.com>
2019-11-14 02:16:42 +0100

    database filler should work somehow

--- database_filler.py
+++ database_filler.py
@@ -8,12 +8,12 @@ import os
 from pathlib import Path
 import mysql.connector
 
-from .common_functions import headers
-from .common_functions import downloadURL
-from .common_functions import sql_run_transaction_and_fetch
-from .common_functions import sql_run_transaction
-from .common_functions import sql_get_result
-
+from common_functions import headers
+from common_functions import downloadURL
+from common_functions import sql_run_transaction_and_fetch
+from common_functions import sql_run_transaction
+from common_functions import sql_get_result
+import update_stops
 
 
 
@@ -90,6 +90,8 @@ def update_json_file(json_data, path, mode, log_message):
 		f.close()
 
 
+def format_shape_traveled(shape_t):
+	return int(float(shape_t) * 1000)
 
 
 if __name__ == "__main__":
@@ -131,23 +133,58 @@ if __name__ == "__main__":
 		for bus_input_list in json_vehiclepositions["features"]:
 			# TODO Trip id se musi zahashovat pro effectivni vyhledavani v databazi
 			trip_id = bus_input_list["properties"]["trip"]["gtfs_trip_id"]
-			result = sql_get_result(mycursor, 'SELECT COUNT(trip_id) FROM trips WHERE trip_id = %s LIMIT 1', (trip_id, ))[0][0]
+			id_trip = sql_get_result(mycursor, 'SELECT id_trip FROM trips WHERE trip_id = %s LIMIT 1', (trip_id, ))
 
 			delay = int(bus_input_list["properties"]["last_position"]["delay"])
-			shape_traveled = int(float(bus_input_list["properties"]["last_position"]["gtfs_shape_dist_traveled"]) * 1000)
+			shape_traveled = format_shape_traveled(bus_input_list["properties"]["last_position"]["gtfs_shape_dist_traveled"])
 			trip_no = bus_input_list["properties"]["trip"]["cis_short_name"]
+			bus_lat = bus_input_list["geometry"]["coordinates"][1]
+			bus_lon = bus_input_list["geometry"]["coordinates"][0]
+			bus_time = bus_input_list["properties"]["last_position"]["origin_timestamp"]
 
-			if result == 0:
+			# if trip does not exist
+			if len(id_trip) == 0:
 				try:
 					json_trip = downloadURL(Request('https://api.golemio.cz/v1/' + trip_id + '?includeShapes=true&includeStops=true', headers=headers))
 				except URLError as e:
 					logging.error("Network error: " + str(e))
-					continue
+					continue  # This jumps to for loop iterating throw all trips in current request.
 
 				trip_headsign = json_trip["trip_headsign"]
-				result = sql_run_transaction_and_fetch(mydb, mycursor, 'SELECT insert_headsign_if_exists_and_return_id(%s)', (trip_headsign, ))[0][0]
-				sql_run_transaction(mydb, mycursor, 'INSERT INTO trips (trip_id, trip_headsign_id, current_delay, shape_traveled, trip_no) VALUES (%s, %s, %s, %s, %s)', (trip_id, result, delay, shape_traveled, trip_no))
 
+				# inserts headsign if not exist and return its id -- (!exists <-> insert) & ret id
+				id_headsign = sql_run_transaction_and_fetch(mydb, mycursor, 'SELECT insert_headsign_if_exists_and_return_id(%s)', (trip_headsign, ))[0][0]
+
+				# inserts trip and return its id
+				id_trip = sql_run_transaction_and_fetch(mydb, mycursor, 'SELECT insert_trip_and_return_id (%s, %s, %s, %s, %s)', (trip_id, id_headsign, delay, shape_traveled, trip_no))
+
+				# creates new ride according to the current trip
+				stops_of_trip = []
+				for json_stop in json_trip["stop_times"]:
+					id_stop = sql_get_result(mycursor, 'SELECT id_stop FROM stops WHERE stop_id = %s', (json_stop["stop_id"], ))
+					if len(id_stop) > 0:
+						id_stop = id_stop[0][0]
+
+					# stop is not in stop table
+					else:
+						update_stops.run_update_stops_script()
+						id_stop = sql_get_result(mycursor, 'SELECT id_stop FROM stops WHERE stop_id = %s', (json_stop["stop_id"],))
+						if len(id_stop) > 0:
+							id_stop = id_stop[0][0]
+
+						# stop was not found in the stop file
+						else:
+							stop_id = json_stop["properties"]["stop_id"]
+							lon = json_stop["geometry"]["coordinates"][0]
+							lat = json_stop["geometry"]["coordinates"][1]
+							stop_name = json_stop["properties"]["stop_name"]
+							id_stop = sql_run_transaction_and_fetch(mydb, mycursor, 'SELECT insert_stop_and_return_id (%s, %s, %s, %s, NULL)', (stop_id, stop_name, lat, lon))
+
+					stops_of_trip.append((id_trip, id_stop, json_stop["arrival_time"], json_stop["departure_time"], format_shape_traveled(json_stop["shape_dist_traveled"])))
+
+				sql_run_transaction(mydb, mycursor, 'INSERT INTO stops (id_trip, id_stop, arrival_time, departure_time, shape_dist_traveled) VALUES (%s, %s, %s, %s, %s)', stops_of_trip)
+
+				# produces shape geojson file
 				geojson_shape = transform_shape_json_file(json_trip)
 				try:
 					update_json_file(geojson_shape, Path(args.trips_folder) / (trip_id + ".shape"), "w+", "New shape of trip " + trip_id + " file exported")
@@ -155,9 +192,15 @@ if __name__ == "__main__":
 					logging.error("Write to file failed: " + str(e))
 					time.sleep(args.update_error - (time.time() - req_start))
 					continue  # This jumps to for loop iterating throw all trips in current request.
+			# trip exists
 			else:
+				id_trip = id_trip[0][0]
+
+				# updates trip data
 				sql_run_transaction(mydb, mycursor, 'UPDATE trips SET current_delay = %s, shape_traveled = %s', (delay, shape_traveled))
 
+			# insert current coordinates
+			sql_run_transaction(mydb, mycursor, 'INSERT INTO trip_coordinates (id_trip, lat, lon, time, delay, shape_traveled) VALUES (%s, %s, %s, %s, %s, %s)', (id_trip, bus_lat, bus_lon, bus_time[:bus_time.index(".")], delay, shape_traveled))
 
 
 		"""
--- update_stops.py
+++ update_stops.py
@@ -17,45 +17,38 @@ class Dictlist(dict):
 		self[key].append(value)
 
 
-def insert(to_insert, id_parent, parent_id):
-	if id_parent == 'NULL':
-		del stops['']
-		sql_run_transaction(mydb, mycursor, 'INSERT IGNORE INTO stops (stop_id, stop_name, lon, lat, parent_id_stop) VALUES (%s, %s, %s, %s, NULL)', [tuple(t) for t in to_insert])
-	else:
-		del stops[parent_id]
-		array_of_records = []
-		for e in to_insert:
-			e.append(id_parent)
-			array_of_records.append(tuple(e))
-		sql_run_transaction(mydb, mycursor, 'INSERT IGNORE INTO stops (stop_id, stop_name, lat, lon, parent_id_stop) VALUES (%s, %s, %s, %s, %s)', array_of_records)
+def insert(stops, to_insert, id_parent, parent_id):
+	mydb = mysql.connector.connect(
+			host="localhost",
+			database="vehicles_info",
+			user="vehicles_access",
+			passwd="my_password"
+		)
+	mycursor = mydb.cursor(prepared=True)
+
+	del stops[parent_id]
+	sql_run_transaction(mydb, mycursor, 'INSERT IGNORE INTO stops (stop_id, stop_name, lat, lon, parent_id_stop) VALUES (%s, %s, %s, %s, ' + str(id_parent) + ')', [tuple(t) for t in to_insert])
 	for e in to_insert:
 		if e[0] in stops:
-			insert(stops[e[0]], sql_get_result(mycursor, 'SELECT id_stop FROM stops WHERE stop_id = %s', (e[0], ))[0][0], e[0])
+			insert(stops, stops[e[0]], sql_get_result(mycursor, 'SELECT id_stop FROM stops WHERE stop_id = %s', (e[0], ))[0][0], e[0])
 
 
+def run_update_stops_script():
 
-mydb = mysql.connector.connect(
-		host="localhost",
-		database="vehicles_info",
-		user="vehicles_access",
-		passwd="my_password"
-	)
-mycursor = mydb.cursor(prepared=True)
+	json_stops = downloadURL(Request('https://api.golemio.cz/v1/gtfs/stops', headers=headers))
+	stops = Dictlist()
 
-json_stops = downloadURL(Request('https://api.golemio.cz/v1/gtfs/stops', headers=headers))
-stops = Dictlist()
-for jstop in json_stops["features"]:
-	stop_id = jstop["properties"]["stop_id"]
-	stop_parent_id = jstop["properties"]["parent_station"]
-	lon = jstop["geometry"]["coordinates"][0]
-	lat = jstop["geometry"]["coordinates"][1]
-	stop_name = jstop["properties"]["stop_name"]
-	stop = [stop_id, stop_name, lon, lat]
-	stops[stop_parent_id] = stop
+	for jstop in json_stops["features"]:
+		stop_id = jstop["properties"]["stop_id"]
+		stop_parent_id = jstop["properties"]["parent_station"]
+		lon = jstop["geometry"]["coordinates"][0]
+		lat = jstop["geometry"]["coordinates"][1]
+		stop_name = jstop["properties"]["stop_name"]
+		stop = [stop_id, stop_name, lat, lon]
+		stops[stop_parent_id] = stop
 
-	#TODO insertuj rekurzivne podle vlozeneho rodice
+	insert(stops, stops[""], 'NULL', '')
 
-insert(stops[""], 'NULL', 'NULL')
 
-	# result = sql_get_result(mycursor, 'SELECT id_stop FROM stops WHERE stop_id = %s', stop_parent_id)[0][0]
-	# sql_run_transaction('INSERT IGNORE INTO stops ()')
+if __name__ == "__main__":
+	run_update_stops_script()
